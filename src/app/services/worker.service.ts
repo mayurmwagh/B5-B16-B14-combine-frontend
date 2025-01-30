30c2d1015c2b1349f589550c508d93641411d6d1
mayurwagh <mayurwagh1412@gmail.com>
2025-01-30 16:29:17 +0530

    pipeline is added and frontend to backend comm eshtablished

--- src/app/services/worker.service.ts
+++ src/app/services/worker.service.ts
@@ -9,7 +9,7 @@ import { Worker } from '../models/worker';
 })
 export class WorkerService {
 
-  private getUrl: string = "http://localhost:8080/api/v1/workers";
+  private getUrl: string = "http://a512417965d8a4bdfa81a06f6cee20a5-122265010.us-east-2.elb.amazonaws.com:8080/api/v1/workers";
 
   constructor(private _httpClient: HttpClient) { }
 
